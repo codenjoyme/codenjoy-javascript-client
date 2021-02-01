4f69207331c7c21b0105f19cb407c99724c01374
Oleksandr_Baglai <apofig@gmail.com>
2021-02-01 23:14:31 +0200

    [js][client] Fixed up and down in all games

--- games/bomberman/direction.js
+++ games/bomberman/direction.js
@@ -1,11 +1,17 @@
 var Direction = require('./../../direction.js');
 
 var BombermanDirection = module.exports = function() {
+
+    // TODO why we need this?
+    var invert = function(dy) {
+        return -dy;
+    }
+
     // move bomberman
     Direction.LEFT = Direction._init(1, -1, 0, 'left');
     Direction.RIGHT = Direction._init(2, 1, 0, 'right');
-    Direction.UP = Direction._init(3, 0, 1, 'up');
-    Direction.DOWN = Direction._init(4, 0, -1, 'down');
+    Direction.UP = Direction._init(3, 0, invert(-1), 'up');
+    Direction.DOWN = Direction._init(4, 0, invert(1), 'down');
 
     // drop the bomb
     Direction.ACT = Direction._init(5, 0, 0, 'act');
--- games/excitebike/direction.js
+++ games/excitebike/direction.js
@@ -2,14 +2,9 @@ var Direction = require('./../../direction.js');
 
 var ExcitebikeDirection = module.exports = function() {
 
-    // TODO why we need this?
-    var invert = function(dy) {
-        return -dy;
-    }
-
     // move hero
-    Direction.UP = Direction._init(1, 0, invert(1), 'up');
-    Direction.DOWN = Direction._init(2, 0, invert(-1), 'down');
+    Direction.UP = Direction._init(1, 0, -1, 'up');
+    Direction.DOWN = Direction._init(2, 0, 1, 'down');
     Direction.STOP = Direction._init(3, 0, 0, '');
 
     return Direction;
