24dafa20274c03548284d0f0db47c3eaac8a5fcc
Oleksandr_Baglai <apofig@gmail.com>
2021-02-01 11:13:24 +0200

    [js][client] Extract direction

--- runner.js
+++ runner.js
@@ -22,6 +22,7 @@
 
 var util = require('util');
 var WSocket = require('ws');
+var Direction = require('direction');
 
 var browser = (browser !== undefined);
 
@@ -143,69 +144,6 @@ var Element = {
 
     /// a void
     NONE : ' '                  // this is the only place where you can move your Bomberman
-};
-
-var D = function(index, dx, dy, name){
-
-    var changeX = function(x) {
-        return x + dx;
-    };
-
-    var changeY = function(y) {
-        return y - dy;
-    };
-
-    var inverted = function() {
-        switch (this) {
-            case Direction.UP : return Direction.DOWN;
-            case Direction.DOWN : return Direction.UP;
-            case Direction.LEFT : return Direction.RIGHT;
-            case Direction.RIGHT : return Direction.LEFT;
-            default : return Direction.STOP;
-        }
-    };
-
-    var toString = function() {
-        return name;
-    };
-
-    return {
-        changeX : changeX,
-
-        changeY : changeY,
-
-        inverted : inverted,
-
-        toString : toString,
-
-        getIndex : function() {
-            return index;
-        }
-    };
-};
-
-var Direction = {
-    UP : D(2, 0, 1, 'up'),                 // you can move
-    DOWN : D(3, 0, -1, 'down'),
-    LEFT : D(0, -1, 0, 'left'),
-    RIGHT : D(1, 1, 0, 'right'),
-    ACT : D(4, 0, 0, 'act'),                // drop bomb
-    STOP : D(5, 0, 0, '')                   // stay
-};
-
-Direction.values = function() {
-   return [Direction.UP, Direction.DOWN, Direction.LEFT, Direction.RIGHT, Direction.ACT, Direction.STOP];
-};
-
-Direction.valueOf = function(index) {
-    var directions = Direction.values();
-    for (var i in directions) {
-        var direction = directions[i];
-        if (direction.getIndex() == index) {
-             return direction;
-        }
-    }
-    return Direction.STOP;
 };
 
 var Point = function (x, y) {
