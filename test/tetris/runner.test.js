332b9f71913ee31032e4dd273ae78c9b8e1c350d
Oleksandr_Baglai <apofig@gmail.com>
2021-02-01 12:52:43 +0200

    [js][client] Moved test class to tetris folder

--- test/tetris/runner.test.js
+++ test/tetris/runner.test.js
@@ -19,7 +19,11 @@
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
-const { Point, Board, Elements } = require('../runner');
+var games = require('games');
+var Direction = require('direction');
+var Point = require('point');
+var Board = games.require('board');
+var Element = games.require('elements');
 
 describe('Point function', () => {
   let point;
