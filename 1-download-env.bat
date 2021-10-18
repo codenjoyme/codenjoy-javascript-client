0c7dfa71e3dd4f63d5a04561e89240ef8eef781d
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-10-18 20:47:21 +0300

    [clients][js] Don't remove NODE_HOME folder just .node.

--- 1-download-env.bat
+++ 1-download-env.bat
@@ -7,9 +7,9 @@ echo        [44;93m+-----------------------------------+[0m
 echo on
 
 cd %ROOT%
-rd /S /Q %NODE_HOME%
 powershell -command "& { set-executionpolicy remotesigned -s currentuser; [System.Net.ServicePointManager]::SecurityProtocol = 3072 -bor 768 -bor 192 -bor 48; $client=New-Object System.Net.WebClient; $client.Headers.Add([System.Net.HttpRequestHeader]::Cookie, 'oraclelicense=accept-securebackup-cookie'); $client.DownloadFile('%ARCH_NODE%','%TOOLS%\node.zip') }"
 %ARCH% x -y -o%TOOLS%\.. %TOOLS%\node.zip
+rd /S /Q .node
 rename %TOOLS%\..\%ARCH_NODE_FOLDER% .node
 cd %ROOT%
 
