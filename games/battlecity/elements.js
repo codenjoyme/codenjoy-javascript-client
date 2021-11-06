6143c4aadfb5297d30f1a33e2d3a205ffc0dba82
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-07 00:26:00 +0200

    [clients][all][battlecity] Added element files.

--- games/battlecity/elements.js
+++ games/battlecity/elements.js
@@ -22,59 +22,202 @@
 
 var BattlecityElement = module.exports = {
 
-    NONE: ' ',
-    BATTLE_WALL: '☼',
-    BANG: 'Ѡ',
+        // An empty space where a tank can move.
 
-    ICE: '#',
-    TREE: '%',
-    RIVER: '~',
+    NONE : ' ',
 
-    WALL: '╬',
+        // The undestroyable wall.
 
-    WALL_DESTROYED_DOWN: '╩',
-    WALL_DESTROYED_UP: '╦',
-    WALL_DESTROYED_LEFT: '╠',
-    WALL_DESTROYED_RIGHT: '╣',
+    BATTLE_WALL : '☼',
 
-    WALL_DESTROYED_DOWN_TWICE: '╨',
-    WALL_DESTROYED_UP_TWICE: '╥',
-    WALL_DESTROYED_LEFT_TWICE: '╞',
-    WALL_DESTROYED_RIGHT_TWICE: '╡',
+        // A destroyed enemy’s tank. A new appears just in a second.
 
-    WALL_DESTROYED_LEFT_RIGHT: '│',
-    WALL_DESTROYED_UP_DOWN: '─',
+    BANG : 'Ѡ',
 
-    WALL_DESTROYED_UP_LEFT: '┌',
-    WALL_DESTROYED_RIGHT_UP: '┐',
-    WALL_DESTROYED_DOWN_LEFT: '└',
-    WALL_DESTROYED_DOWN_RIGHT: '┘',
+        // Ice, having driven onto which the tank will begin to drift.
+        // During a skid, the tank will repeat the old commands for
+        // several ticks in a row, ignoring the current commands.
 
-    WALL_DESTROYED: ' ',
+    ICE : '#',
 
-    BULLET: '•',
+        // The trees hide tanks which can continue to shoot at the same
+        // time. The fired shells are also not visible under the trees.
+        // Only prizes can be seen from behind the trees.
 
-    TANK_UP: '▲',
-    TANK_RIGHT: '►',
-    TANK_DOWN: '▼',
-    TANK_LEFT: '◄',
+    TREE : '%',
 
-    OTHER_TANK_UP: '˄',
-    OTHER_TANK_RIGHT: '˃',
-    OTHER_TANK_DOWN: '˅',
-    OTHER_TANK_LEFT: '˂',
+        // The river does not allow to pass through itself without the
+        // PRIZE_WALKING_ON_WATER prize, but the shells fly freely
+        // through the water. A tank stuck in the middle of the water,
+        // after canceling the PRIZE_WALKING_ON_WATER prize, can move 1
+        // cell in the water only every N ticks.
 
-    AI_TANK_UP: '?',
-    AI_TANK_RIGHT: '»',
-    AI_TANK_DOWN: '¿',
-    AI_TANK_LEFT: '«',
+    RIVER : '~',
 
-    AI_TANK_PRIZE: '◘',
+        // A wall that hasn't been shot yet. It takes 3 shots to
+        // completely destroy.
 
-    PRIZE: '!',
-    PRIZE_IMMORTALITY: '1',
-    PRIZE_BREAKING_WALLS: '2',
-    PRIZE_WALKING_ON_WATER: '3',
-    PRIZE_VISIBILITY: '4',
-    PRIZE_NO_SLIDING: '5'
+    WALL : '╬',
+
+        // Partially destroyed wall. For complete destruction, 2 shot
+        // is required.
+
+    WALL_DESTROYED_DOWN : '╩',
+
+        // Partially destroyed wall. For complete destruction, 2 shot
+        // is required.
+
+    WALL_DESTROYED_UP : '╦',
+
+        // Partially destroyed wall. For complete destruction, 2 shot
+        // is required.
+
+    WALL_DESTROYED_LEFT : '╠',
+
+        // Partially destroyed wall. For complete destruction, 2 shot
+        // is required.
+
+    WALL_DESTROYED_RIGHT : '╣',
+
+        // Partially destroyed wall. For complete destruction, 1 shot
+        // is required.
+
+    WALL_DESTROYED_DOWN_TWICE : '╨',
+
+        // Partially destroyed wall. For complete destruction, 1 shot
+        // is required.
+
+    WALL_DESTROYED_UP_TWICE : '╥',
+
+        // Partially destroyed wall. For complete destruction, 1 shot
+        // is required.
+
+    WALL_DESTROYED_LEFT_TWICE : '╞',
+
+        // Partially destroyed wall. For complete destruction, 1 shot
+        // is required.
+
+    WALL_DESTROYED_RIGHT_TWICE : '╡',
+
+        // Partially destroyed wall. For complete destruction, 1 shot
+        // is required.
+
+    WALL_DESTROYED_LEFT_RIGHT : '│',
+
+        // Partially destroyed wall. For complete destruction, 1 shot
+        // is required.
+
+    WALL_DESTROYED_UP_DOWN : '─',
+
+        // Partially destroyed wall. For complete destruction, 1 shot
+        // is required.
+
+    WALL_DESTROYED_UP_LEFT : '┌',
+
+        // Partially destroyed wall. For complete destruction, 1 shot
+        // is required.
+
+    WALL_DESTROYED_RIGHT_UP : '┐',
+
+        // Partially destroyed wall. For complete destruction, 1 shot
+        // is required.
+
+    WALL_DESTROYED_DOWN_LEFT : '└',
+
+        // Partially destroyed wall. For complete destruction, 1 shot
+        // is required.
+
+    WALL_DESTROYED_DOWN_RIGHT : '┘',
+
+        // Partially destroyed wall. For complete destruction, 2 shot
+        // is required.
+
+    WALL_DESTROYED : ' ',
+
+        // Completely destroyed wall. Wall will recover over time.
+
+    BULLET : '•',
+
+        // Your tank is pointing up.
+
+    TANK_UP : '▲',
+
+        // Your tank is pointing right.
+
+    TANK_RIGHT : '►',
+
+        // Your tank is pointing down.
+
+    TANK_DOWN : '▼',
+
+        // Your tank is pointing left.
+
+    TANK_LEFT : '◄',
+
+        // Enemy tank is pointing up.
+
+    OTHER_TANK_UP : '˄',
+
+        // Enemy tank is pointing right.
+
+    OTHER_TANK_RIGHT : '˃',
+
+        // Enemy tank is pointing down.
+
+    OTHER_TANK_DOWN : '˅',
+
+        // Enemy tank is pointing left.
+
+    OTHER_TANK_LEFT : '˂',
+
+        // AI-tank is pointing up.
+
+    AI_TANK_UP : '?',
+
+        // AI-tank is pointing right.
+
+    AI_TANK_RIGHT : '»',
+
+        // AI-tank is pointing down.
+
+    AI_TANK_DOWN : '¿',
+
+        // AI-tank is pointing left.
+
+    AI_TANK_LEFT : '«',
+
+        // AI-tank can also be a prize, then it is highlighted by this
+        // sprite every few ticks.
+
+    AI_TANK_PRIZE : '◘',
+
+        // The dropped prize after the destruction of the prize tank
+        // flickers on the field every even tick of the game with this
+        // sprite.
+
+    PRIZE : '!',
+
+        // A prize that gives the hero temporary invulnerability.
+
+    PRIZE_IMMORTALITY : '1',
+
+        // A prize that allows you to temporarily destroy any walls
+        // with 1 shot, even indestructible ones (but not the border of
+        // the field).
+
+    PRIZE_BREAKING_WALLS : '2',
+
+        // A prize that allows the hero to temporarily walk on water.
+
+    PRIZE_WALKING_ON_WATER : '3',
+
+        // A prize that allows the hero to temporarily see all enemies
+        // under the trees.
+
+    PRIZE_VISIBILITY : '4',
+
+        // A prize that allows the hero to temporarily not slide on the
+        // ice.
+
+    PRIZE_NO_SLIDING : '5'
 }
