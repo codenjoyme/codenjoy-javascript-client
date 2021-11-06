f1e06ebc5a90eb63829c9ba47610aeb0883c032a
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-07 00:26:35 +0200

    [clients][all][sample] Added element files.

---
+++ games/sample/elements.js
@@ -0,0 +1,56 @@
+/*-
+ * #%L
+ * Codenjoy - it's a dojo-like platform from developers to developers.
+ * %%
+ * Copyright (C) 2021 Codenjoy
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+var SampleElement = module.exports = {
+
+        // Empty place where the hero can go.
+
+    NONE : ' ',
+
+        // Wall you can't walk through.
+
+    WALL : '☼',
+
+        // Your hero.
+
+    HERO : '☺',
+
+        // Heroes of other players.
+
+    OTHER_HERO : '☻',
+
+        // Your hero died. His body will disappear in the next tick.
+
+    DEAD_HERO : 'X',
+
+        // Another player's hero died.
+
+    OTHER_DEAD_HERO : 'Y',
+
+        // Gold. It must be picked up.
+
+    GOLD : '$',
+
+        // Bomb planted by the hero. You can blow up on it.
+
+    BOMB : 'x'
+}
