82c111e96cb2cd1a06866a6b9ed5d69cccb6d7e6
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-08 00:46:57 +0200

    [clients][js][mollymage] Remove duplicate in board class. Extend from engine/board.js.

--- games/mollymage/board.js
+++ games/mollymage/board.js
@@ -22,181 +22,97 @@
 
 var MollyMageBoard = module.exports = function(board){
 
+    var Board = require('./../../engine/board.js');
     var Games = require('./../../engine/games.js');
-    var Direction = Games.require('./engine/direction.js');
     var Point = require('./../../engine/point.js');
-    var util = require('util');
     var Stuff = require('./../../engine/stuff.js');
     var Element = Games.require('./elements.js');
-    var LengthToXY = require('./../../engine/lxy.js');
 
-    var contains = function(a, obj) {
-        var i = a.length;
-        while (i--) {
-            if (a[i].equals(obj)) {
-                return true;
-            }
-        }
-        return false;
-    };
+    var board = new Board(board);
 
-    var removeDuplicates = function(all) {
+    board.getHero = function() {
         var result = [];
-        for (var index in all) {
-            var point = all[index];
-            if (!contains(result, point)) {
-                result.push(point);
-            }
-        }
-        return result;
-    };
-
-    var boardSize = function() {
-        return Math.sqrt(board.length);
-    };
-
-    var size = boardSize();
-    var xyl = new LengthToXY(size);
-
-    var getHero = function() {
-        var result = [];
-        result = result.concat(findAll(Element.HERO));
-        result = result.concat(findAll(Element.POTION_HERO));
-        result = result.concat(findAll(Element.DEAD_HERO));
+        result = result.concat(board.findAll(Element.HERO));
+        result = result.concat(board.findAll(Element.POTION_HERO));
+        result = result.concat(board.findAll(Element.DEAD_HERO));
         return result[0];
     };
 
-    var getOtherHeroes = function() {
+    board.getOtherHeroes = function() {
         var result = [];
-        result = result.concat(findAll(Element.OTHER_HERO));
-        result = result.concat(findAll(Element.OTHER_POTION_HERO));
-        result = result.concat(findAll(Element.OTHER_DEAD_HERO));
+        result = result.concat(board.findAll(Element.OTHER_HERO));
+        result = result.concat(board.findAll(Element.OTHER_POTION_HERO));
+        result = result.concat(board.findAll(Element.OTHER_DEAD_HERO));
         return result;
     };
 
-    var getEnemyHeroes = function() {
+    board.getEnemyHeroes = function() {
         var result = [];
-        result = result.concat(findAll(Element.ENEMY_HERO));
-        result = result.concat(findAll(Element.ENEMY_POTION_HERO));
-        result = result.concat(findAll(Element.ENEMY_DEAD_HERO));
+        result = result.concat(board.findAll(Element.ENEMY_HERO));
+        result = result.concat(board.findAll(Element.ENEMY_POTION_HERO));
+        result = result.concat(board.findAll(Element.ENEMY_DEAD_HERO));
         return result;
     };
 
-    var isMyHeroDead = function() {
-        return board.indexOf(Element.DEAD_HERO) != -1;
-    };
-
-    var isAt = function(x, y, element) {
-        if (new Point(x, y).isOutOf(size)) {
-            return false;
-        }
-        return getAt(x, y) == element;
-    };
-
-    var getAt = function(x, y) {
-        if (new Point(x, y).isOutOf(size)) {
-            return Element.WALL;
-        }
-        return board.charAt(xyl.getLength(x, y));
-    };
-
-    var boardAsString = function() {
-        var result = "";
-        for (var i = 0; i < size; i++) {
-            result += board.substring(i * size, (i + 1) * size);
-            result += "\n";
-        }
-        return result;
-    };
-
-    var getBarriers = function() {
-        var all = getGhosts();
-        all = all.concat(getWalls());
-        all = all.concat(getPotions());
-        all = all.concat(getTreasureBoxes());
-        all = all.concat(getOtherHeroes());
-        all = all.concat(getEnemyHeroes());
-        all = all.concat(getFutureBlasts());
-        return removeDuplicates(all);
-    };
-
-    var toString = function() {
-        return util.format("%s\n" +
-            "Hero at: %s\n" +
-            "Other heroes at: %s\n" +
-            "Enemy heroes at: %s\n" +
-            "Ghosts at: %s\n" +
-            "Treasure boxes at: %s\n" +
-            "Potions at: %s\n" +
-            "Blasts: %s\n" +
-            "Expected blasts at: %s\n" +
-            "Perks at: %s",
-            boardAsString(),
-            getHero(),
-            Stuff.printArray(getOtherHeroes()),
-            Stuff.printArray(getEnemyHeroes()),
-            Stuff.printArray(getGhosts()),
-            Stuff.printArray(getTreasureBoxes()),
-            Stuff.printArray(getPotions()),
-            Stuff.printArray(getBlasts()),
-            Stuff.printArray(getFutureBlasts()),
-            Stuff.printArray(getPerks())
-        );
-    };
-
-    var getGhosts = function() {
-        return findAll(Element.GHOST);
-    };
-
-    var findAll = function(element) {
+    board.isGameOver = function() {
         var result = [];
-        for (var i = 0; i < size*size; i++) {
-            var point = xyl.getXY(i);
-            if (isAt(point.getX(), point.getY(), element)) {
-                result.push(point);
-            }
-        }
-        return result;
+        result = result.concat(board.findAll(Element.DEAD_HERO));
+        return result.length > 0;
     };
 
-    var getWalls = function() {
-        return findAll(Element.WALL);
+    board.getBarriers = function() {
+        var all = board.getGhosts();
+        all = all.concat(board.getWalls());
+        all = all.concat(board.getPotions());
+        all = all.concat(board.getTreasureBoxes());
+        all = all.concat(board.getOtherHeroes());
+        all = all.concat(board.getEnemyHeroes());
+        all = all.concat(board.getFutureBlasts());
+        return board.removeDuplicates(all);
     };
 
-    var getTreasureBoxes = function() {
-        return findAll(Element.TREASURE_BOX);
+    board.getGhosts = function() {
+        return board.findAll(Element.GHOST);
     };
 
-    var getPotions = function() {
+    board.getWalls = function() {
+        return board.findAll(Element.WALL);
+    };
+
+    board.getTreasureBoxes = function() {
+        return board.findAll(Element.TREASURE_BOX);
+    };
+
+    board.getPotions = function() {
         var result = [];
-        result = result.concat(findAll(Element.POTION_TIMER_1));
-        result = result.concat(findAll(Element.POTION_TIMER_2));
-        result = result.concat(findAll(Element.POTION_TIMER_3));
-        result = result.concat(findAll(Element.POTION_TIMER_4));
-        result = result.concat(findAll(Element.POTION_TIMER_5));
-        result = result.concat(findAll(Element.POTION_HERO));
-        result = result.concat(findAll(Element.OTHER_POTION_HERO));
-        result = result.concat(findAll(Element.ENEMY_POTION_HERO));
+        result = result.concat(board.findAll(Element.POTION_TIMER_1));
+        result = result.concat(board.findAll(Element.POTION_TIMER_2));
+        result = result.concat(board.findAll(Element.POTION_TIMER_3));
+        result = result.concat(board.findAll(Element.POTION_TIMER_4));
+        result = result.concat(board.findAll(Element.POTION_TIMER_5));
+        result = result.concat(board.findAll(Element.POTION_HERO));
+        result = result.concat(board.findAll(Element.OTHER_POTION_HERO));
+        result = result.concat(board.findAll(Element.ENEMY_POTION_HERO));
         return result;
     };
 
-    var getPerks = function() {
+    board.getPerks = function() {
         var result = [];
-        result = result.concat(findAll(Element.POTION_BLAST_RADIUS_INCREASE));
-        result = result.concat(findAll(Element.POTION_COUNT_INCREASE));
-        result = result.concat(findAll(Element.POTION_REMOTE_CONTROL));
-        result = result.concat(findAll(Element.POTION_IMMUNE));
-        result = result.concat(findAll(Element.POISON_THROWER));
-        result = result.concat(findAll(Element.POTION_EXPLODER));
+        result = result.concat(board.findAll(Element.POTION_BLAST_RADIUS_INCREASE));
+        result = result.concat(board.findAll(Element.POTION_COUNT_INCREASE));
+        result = result.concat(board.findAll(Element.POTION_REMOTE_CONTROL));
+        result = result.concat(board.findAll(Element.POTION_IMMUNE));
+        result = result.concat(board.findAll(Element.POISON_THROWER));
+        result = result.concat(board.findAll(Element.POTION_EXPLODER));
         return result;
-    }
-
-    var getBlasts = function() {
-        return findAll(Element.BOOM);
     };
 
-    var getFutureBlasts = function() {
-        var potions = getPotions();
+    board.getBlasts = function() {
+        return board.findAll(Element.BOOM);
+    };
+
+    board.getFutureBlasts = function() {
+        var potions = board.getPotions();
         var result = [];
         for (var index in potions) {
             var potion = potions[index];
@@ -209,71 +125,45 @@ var MollyMageBoard = module.exports = function(board){
         var result2 = [];
         for (var index in result) {
             var blast = result[index];
-            if (blast.isOutOf(size) || contains(getWalls(), blast)) {
+            if (blast.isOutOf(size) || board.contains(board.getWalls(), blast)) {
                 continue;
             }
             result2.push(blast);
         }
-        return removeDuplicates(result2);
+        return board.removeDuplicates(result2);
     };
 
-    var isAnyOfAt = function(x, y, elements) {
-        for (var index in elements) {
-            var element = elements[index];
-            if (isAt(x, y,element)) {
-                return true;
-            }
+    board.isBarrierAt = function(x, y) {
+        if (new Point(x, y).isOutOf(board.size)) {
+            return true;
         }
-        return false;
+
+        return board.contains(board.getBarriers(), new Point(x, y));
     };
 
-    var isNear = function(x, y, element) {
-        if (new Point(x, y).isOutOf(size)) {
-            return false;
-        }
-        return isAt(x + 1, y, element) || // TODO to remove duplicate
-            isAt(x - 1, y, element) ||
-            isAt(x, y + 1, element) ||
-            isAt(x, y - 1, element);
+    board.toString = function() {
+        return Stuff.format("%s\n" +
+            "Hero at: %s\n" +
+            "Other heroes at: %s\n" +
+            "Enemy heroes at: %s\n" +
+            "Ghosts at: %s\n" +
+            "Treasure boxes at: %s\n" +
+            "Potions at: %s\n" +
+            "Blasts: %s\n" +
+            "Expected blasts at: %s\n" +
+            "Perks at: %s",
+            board.boardAsString(),
+            board.getHero(),
+            board.getOtherHeroes(),
+            board.getEnemyHeroes(),
+            board.getGhosts(),
+            board.getTreasureBoxes(),
+            board.getPotions(),
+            board.getBlasts(),
+            board.getFutureBlasts(),
+            board.getPerks()
+        );
     };
 
-    var isBarrierAt = function(x, y) {
-        return contains(getBarriers(), new Point(x, y));
-    };
-
-    var countNear = function(x, y, element) {
-        if (new Point(x, y).isOutOf(size)) {
-            return 0;
-        }
-        var count = 0;
-        if (isAt(x - 1, y    , element)) count ++; // TODO to remove duplicate
-        if (isAt(x + 1, y    , element)) count ++;
-        if (isAt(x    , y - 1, element)) count ++;
-        if (isAt(x    , y + 1, element)) count ++;
-        return count;
-    };
-
-    return {
-        size : boardSize,
-        getHero : getHero,
-        getOtherHeroes : getOtherHeroes,
-        isMyHeroDead : isMyHeroDead,
-        isAt : isAt,
-        boardAsString : boardAsString,
-        getBarriers : getBarriers,
-        toString : toString,
-        getGhosts : getGhosts,
-        findAll : findAll,
-        getWalls : getWalls,
-        getTreasureBoxes : getTreasureBoxes,
-        getPotions : getPotions,
-        getBlasts : getBlasts,
-        getFutureBlasts : getFutureBlasts,
-        isAnyOfAt : isAnyOfAt,
-        isNear : isNear,
-        isBarrierAt : isBarrierAt,
-        countNear : countNear,
-        getAt : getAt,
-        getPerks: getPerks
-    };
+    return board;
 };
