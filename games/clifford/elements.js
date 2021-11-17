94bbb80942afd17f93c30c361f2cf7e604724ac8
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-17 09:27:15 +0200

    [all][clifford][element] Changed sprites chars.

--- games/clifford/elements.js
+++ games/clifford/elements.js
@@ -76,11 +76,11 @@ var CliffordElement = module.exports = {
         // Your hero is dead. In the next tick, it will disappear and
         // appear in a new location.
 
-    HERO_DIE : 'Ѡ',
+    HERO_DIE : 'O',
 
         // Your hero is climbing the ladder.
 
-    HERO_LADDER : 'Y',
+    HERO_LADDER : 'A',
 
         // Your hero runs to the left.
 
@@ -92,225 +92,225 @@ var CliffordElement = module.exports = {
 
         // Your hero is falling.
 
-    HERO_FALL : ']',
+    HERO_FALL : 'U',
 
         // Your hero is crawling along the pipe.
 
-    HERO_PIPE : '{',
+    HERO_PIPE : 'I',
 
         // Your hero in the pit.
 
-    HERO_PIT : '⍃',
+    HERO_PIT : 'E',
 
         // Your shadow-hero is dead. In the next tick, it will
         // disappear and appear in a new location.
 
-    HERO_MASK_DIE : 'x',
+    HERO_MASK_DIE : 'o',
 
         // Your shadow-hero is climbing the ladder.
 
-    HERO_MASK_LADDER : '⍬',
+    HERO_MASK_LADDER : 'a',
 
         // Your shadow-hero runs to the left.
 
-    HERO_MASK_LEFT : '⊲',
+    HERO_MASK_LEFT : 'h',
 
         // Your shadow-hero runs to the right.
 
-    HERO_MASK_RIGHT : '⊳',
+    HERO_MASK_RIGHT : 'w',
 
         // Your shadow-hero is falling.
 
-    HERO_MASK_FALL : '⊅',
+    HERO_MASK_FALL : 'u',
 
         // Your shadow-hero is crawling along the pipe.
 
-    HERO_MASK_PIPE : '⋜',
+    HERO_MASK_PIPE : 'i',
 
         // Your shadow-hero in the pit.
 
-    HERO_MASK_PIT : 'ᐊ',
+    HERO_MASK_PIT : 'e',
 
         // Other hero is dead. In the next tick, it will disappear and
         // appear in a new location.
 
-    OTHER_HERO_DIE : 'Z',
+    OTHER_HERO_DIE : 'C',
 
         // Other hero is climbing the ladder.
 
-    OTHER_HERO_LADDER : 'U',
+    OTHER_HERO_LADDER : 'D',
 
         // Other hero runs to the left.
 
-    OTHER_HERO_LEFT : ')',
+    OTHER_HERO_LEFT : '«',
 
         // Other hero runs to the right.
 
-    OTHER_HERO_RIGHT : '(',
+    OTHER_HERO_RIGHT : '»',
 
         // Other hero is falling.
 
-    OTHER_HERO_FALL : '⊐',
+    OTHER_HERO_FALL : 'F',
 
         // Other hero is crawling along the pipe.
 
-    OTHER_HERO_PIPE : 'Э',
+    OTHER_HERO_PIPE : 'J',
 
         // Other hero in the pit.
 
-    OTHER_HERO_PIT : 'ᗉ',
+    OTHER_HERO_PIT : 'K',
 
         // Other shadow-hero is dead. In the next tick, it will
         // disappear and appear in a new location.
 
-    OTHER_HERO_MASK_DIE : '⋈',
+    OTHER_HERO_MASK_DIE : 'c',
 
         // Other shadow-hero is climbing the ladder.
 
-    OTHER_HERO_MASK_LADDER : '⋕',
+    OTHER_HERO_MASK_LADDER : 'd',
 
         // Other shadow-hero runs to the left.
 
-    OTHER_HERO_MASK_LEFT : '⋊',
+    OTHER_HERO_MASK_LEFT : 'Z',
 
         // Other shadow-hero runs to the right.
 
-    OTHER_HERO_MASK_RIGHT : '⋉',
+    OTHER_HERO_MASK_RIGHT : 'z',
 
         // Other shadow-hero is falling.
 
-    OTHER_HERO_MASK_FALL : '⋣',
+    OTHER_HERO_MASK_FALL : 'f',
 
         // Other shadow-hero is crawling along the pipe.
 
-    OTHER_HERO_MASK_PIPE : '⊣',
+    OTHER_HERO_MASK_PIPE : 'j',
 
         // Other shadow-hero in the pit.
 
-    OTHER_HERO_MASK_PIT : 'ᗏ',
+    OTHER_HERO_MASK_PIT : 'k',
 
         // Enemy hero is dead. In the next tick, it will disappear and
         // appear in a new location.
 
-    ENEMY_HERO_DIE : 'Ž',
+    ENEMY_HERO_DIE : 'L',
 
         // Enemy hero is climbing the ladder.
 
-    ENEMY_HERO_LADDER : 'Ǔ',
+    ENEMY_HERO_LADDER : 'N',
 
         // Enemy hero runs to the left.
 
-    ENEMY_HERO_LEFT : '❫',
+    ENEMY_HERO_LEFT : 'P',
 
         // Enemy hero runs to the right.
 
-    ENEMY_HERO_RIGHT : '❪',
+    ENEMY_HERO_RIGHT : 'Q',
 
         // Enemy hero is falling.
 
-    ENEMY_HERO_FALL : '⋥',
+    ENEMY_HERO_FALL : 'R',
 
         // Enemy hero is crawling along the pipe.
 
-    ENEMY_HERO_PIPE : 'Ǯ',
+    ENEMY_HERO_PIPE : 'T',
 
         // Enemy hero in the pit.
 
-    ENEMY_HERO_PIT : '⇇',
+    ENEMY_HERO_PIT : 'V',
 
         // Enemy shadow-hero is dead. In the next tick, it will
         // disappear and appear in a new location.
 
-    ENEMY_HERO_MASK_DIE : '⧓',
+    ENEMY_HERO_MASK_DIE : 'l',
 
         // Enemy shadow-hero is climbing the ladder.
 
-    ENEMY_HERO_MASK_LADDER : '≠',
+    ENEMY_HERO_MASK_LADDER : 'n',
 
         // Enemy shadow-hero runs to the left.
 
-    ENEMY_HERO_MASK_LEFT : '⧒',
+    ENEMY_HERO_MASK_LEFT : 'p',
 
         // Enemy shadow-hero runs to the right.
 
-    ENEMY_HERO_MASK_RIGHT : '⧑',
+    ENEMY_HERO_MASK_RIGHT : 'q',
 
         // Enemy shadow-hero is falling.
 
-    ENEMY_HERO_MASK_FALL : '⌫',
+    ENEMY_HERO_MASK_FALL : 'r',
 
         // Enemy shadow-hero is crawling along the pipe.
 
-    ENEMY_HERO_MASK_PIPE : '❵',
+    ENEMY_HERO_MASK_PIPE : 't',
 
         // Enemy shadow-hero in the pit.
 
-    ENEMY_HERO_MASK_PIT : '⬱',
+    ENEMY_HERO_MASK_PIT : 'v',
 
         // Robber is climbing the ladder.
 
-    ROBBER_LADDER : 'Q',
+    ROBBER_LADDER : 'X',
 
         // Robber runs to the left. Robber picks up the nearest prey
         // and hunts for it until it overtakes it.
 
-    ROBBER_LEFT : '«',
+    ROBBER_LEFT : ')',
 
         // Robber runs to the right. Robber picks up the nearest prey
         // and hunts for it until it overtakes it.
 
-    ROBBER_RIGHT : '»',
+    ROBBER_RIGHT : '(',
 
         // Robber is falling.
 
-    ROBBER_FALL : '‹',
+    ROBBER_FALL : 'x',
 
         // Robber is crawling along the pipe.
 
-    ROBBER_PIPE : '<',
+    ROBBER_PIPE : 'Y',
 
         // Robber in the pit.
 
-    ROBBER_PIT : '⍇',
+    ROBBER_PIT : 'y',
 
         // Opened golden gates. Can only be locked with a golden key.
 
-    OPENED_DOOR_GOLD : '⍙',
+    OPENED_DOOR_GOLD : 'g',
 
         // Opened silver gates. Can only be locked with a silver key.
 
-    OPENED_DOOR_SILVER : '⍚',
+    OPENED_DOOR_SILVER : 's',
 
         // Opened bronze gates. Can only be locked with a bronze key.
 
-    OPENED_DOOR_BRONZE : '⍜',
+    OPENED_DOOR_BRONZE : 'b',
 
         // Closed golden gates. Can only be opened with a golden key.
 
-    CLOSED_DOOR_GOLD : '⍍',
+    CLOSED_DOOR_GOLD : 'G',
 
         // Closed silver gates. Can only be opened with a silver key.
 
-    CLOSED_DOOR_SILVER : '⌺',
+    CLOSED_DOOR_SILVER : 'S',
 
         // Closed bronze gates. Can only be opened with a bronze key.
 
-    CLOSED_DOOR_BRONZE : '⌼',
+    CLOSED_DOOR_BRONZE : 'B',
 
         // Bronze key. Helps open/close golden gates. The key can only
         // be used once.
 
-    KEY_GOLD : '✦',
+    KEY_GOLD : '+',
 
         // Silver key. Helps open/close silver gates. The key can only
         // be used once.
 
-    KEY_SILVER : '✼',
+    KEY_SILVER : '-',
 
         // Bronze key. Helps open/close bronze gates. The key can only
         // be used once.
 
-    KEY_BRONZE : '⍟',
+    KEY_BRONZE : '!',
 
         // Bullet. After the shot by the hero, the bullet flies until
         // it meets an obstacle. The bullet kills the hero. It
@@ -331,10 +331,10 @@ var CliffordElement = module.exports = {
         // Back door - allows the hero to secretly move to another
         // random place on the map.
 
-    BACKWAY : '⊛',
+    BACKWAY : 'W',
 
         // Disguise potion - endow the hero with additional abilities.
         // The hero goes into shadow mode.
 
-    MASK_POTION : 'S'
+    MASK_POTION : 'm'
 }
