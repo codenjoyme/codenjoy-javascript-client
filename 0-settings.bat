db6b9aba167328041adac22ab62694e921e0afd5
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-19 19:09:53 +0200

    [clients][all][bat] Reformat.

--- 0-settings.bat
+++ 0-settings.bat
@@ -7,7 +7,7 @@ if "%BOARD_URL%"==""   ( set BOARD_URL=http://127.0.0.1:8080/codenjoy-contest/bo
 
 set ROOT=%CD%
 
-if "%SKIP_TESTS%"=="" ( set SKIP_TESTS=true)
+if "%SKIP_TESTS%"==""  ( set SKIP_TESTS=true)
 
 set CODE_PAGE=65001
 chcp %CODE_PAGE%
@@ -16,9 +16,10 @@ set TOOLS=%ROOT%\.tools
 set ARCH=%TOOLS%\7z\7za.exe
 
 rem Set to true if you want to ignore node installation on the system
-if "%INSTALL_LOCALLY%"=="" ( set INSTALL_LOCALLY=true)
+if "%INSTALL_LOCALLY%"==""     ( set INSTALL_LOCALLY=true)
 
 if "%INSTALL_LOCALLY%"=="true" ( set NODE_HOME=)
+
 if "%NODE_HOME%"=="" ( set NODE_HOME=%ROOT%\.node)
 
 set NPM=%NODE_HOME%\npm
