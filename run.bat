4a13ebf8f942c3d6bd295f7f8d2c0d94d11175f1
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-21 14:12:04 +0200

    [clients][js][bat] Fixed run.bat so it only contains the abstract part common to all clients. All specific step instructions are contained in stuff.bat.

--- run.bat
+++ run.bat
@@ -1,24 +1,29 @@
 @echo off
 
-rem red *;91
-rem green *;92
-rem yellow *;93
-rem blue *;94
-rem pink *;95
-rem light blue *;96
-rem purple *;97
-rem black background 40;*
-rem dark yellow background 43;*
-rem purple background 45;*
-rem blue background 44;*
-set CL_HEADER=43;93
-set CL_COMMAND=40;96
-set CL_QUESTION=45;97
-set CL_INFO=44;93
+:check_run_mode
+    if "%*"=="" (       
+        call :run_executable 
+    ) else (
+        call :run_library %*
+    )
+    goto :eof
 
-call :settings
+:run_executable
+    rem run run.bat as executable script
+    goto :init
 
-set OPTION=%1
+:run_library
+    rem run run.bat as library
+    set input=%* 
+    rem replace all ‘ with "
+    call set all=%%input:‘="%%
+    call %all%
+    goto :eof      
+
+:init
+    call :init_colors    
+    call :settings    
+    set OPTION=%1
 
 :start
     if "%OPTION%"=="" (
@@ -31,12 +36,30 @@ set OPTION=%1
     set OPTION=
     goto :start
 
+:init_colors 
+    rem red *;91
+    rem green *;92
+    rem yellow *;93
+    rem blue *;94
+    rem pink *;95
+    rem light blue *;96
+    rem purple *;97
+    rem black background 40;*
+    rem dark yellow background 43;*
+    rem purple background 45;*
+    rem blue background 44;*
+    set CL_HEADER=43;93
+    set CL_COMMAND=40;96
+    set CL_QUESTION=45;97
+    set CL_INFO=44;93
+    goto :eof  
+
 :eval_echo
     set input=%~1%
-    call set command=%%input:`="%%
+    rem replace all “ with "
+    call set command=%%input:“="%%
     call :color "%CL_COMMAND%" "%input%"
     call %command%
-
     goto :eof
 
 :ask_option
@@ -47,7 +70,6 @@ set OPTION=%1
     if "%CODE%"=="t" set OPTION=test
     if "%CODE%"=="r" set OPTION=run
     if "%CODE%"=="q" exit
-
     goto :eof
 
 :read_env
@@ -56,7 +78,6 @@ set OPTION=%1
         SET %%i
         call :color "%CL_INFO%" "%%i"
     )
-
     goto :eof
 
 :print_color
@@ -66,7 +87,6 @@ set OPTION=%1
          call :color "%CL_INFO%" "%%s"
     )
     del /Q %TOOLS%\out
-
     goto :eof
 
 :color
@@ -74,18 +94,15 @@ set OPTION=%1
     set message=%~2%
     echo [%color%m%message%[0m
     echo.
-
     goto :eof
 
 :ask
     call :color "%CL_QUESTION%" "Press any key to continue"
     pause >nul
-
     goto :eof
 
 :sep
     call :color "%CL_COMMAND%" "---------------------------------------------------------------------------------------"
-
     goto :eof
 
 :download_file
@@ -94,7 +111,6 @@ set OPTION=%1
     call :color "%CL_COMMAND%" "Downloading '%url%'"
     call :color "%CL_COMMAND%" "       into '%file%'"
     powershell -command "& { set-executionpolicy remotesigned -s currentuser; [System.Net.ServicePointManager]::SecurityProtocol = 3072 -bor 768 -bor 192 -bor 48; $client=New-Object System.Net.WebClient; $client.Headers.Add([System.Net.HttpRequestHeader]::Cookie, 'oraclelicense=accept-securebackup-cookie'); $client.DownloadFile('%url%','%file%') }"
-
     goto :eof
 
 :install
@@ -115,77 +131,41 @@ set OPTION=%1
         call :eval_echo "rename %TOOLS%\..\%FOLDER% .%DEST%"
     )
     call :eval_echo "cd %ROOT%"
-
     goto :eof
 
 :settings
     call :color "%CL_HEADER%" "Setup variables..."
-
     call :read_env
-
     set ROOT=%CD%
-
     if "%SKIP_TESTS%"==""  ( set SKIP_TESTS=true)
-
     set CODE_PAGE=65001
     chcp %CODE_PAGE%
-
     set TOOLS=%ROOT%\.tools
     set ARCH=%TOOLS%\7z\7za.exe
-
-    rem Set to true if you want to ignore node installation on the system
+    rem Set to true if you want to ignore platform installation on the system
     if "%INSTALL_LOCALLY%"==""     ( set INSTALL_LOCALLY=true)
-
-    if "%INSTALL_LOCALLY%"=="true" ( set NODE_HOME=)
-
-    if "%NODE_HOME%"==""   ( set NO_NODE=true)
-    if "%NO_NODE%"=="true" ( set NODE_HOME=%ROOT%\.node)
-    if "%NO_NODE%"=="true" ( set PATH=%NODE_HOME%\;%PATH%)
-
-    set NPM=%NODE_HOME%\npm
-
-    echo Language enviromnent variables
-    call :color "%CL_INFO%" "PATH=%PATH%"
-    call :color "%CL_INFO%" "NODE_HOME=%NODE_HOME%"
-
-    set ARCH_URL=https://nodejs.org/dist/v14.17.0/node-v14.17.0-win-x64.zip
-    set ARCH_FOLDER=node-v14.17.0-win-x64
-
+    call stuff :settings
     goto :eof
 
 :download
     call :color "%CL_HEADER%" "Installing..."
-
-    if exist %TOOLS%\..\node_modules (
-        call :eval_echo "rd /S /Q %TOOLS%\..\node_modules"
-    )
-
-    if     "%INSTALL_LOCALLY%"=="true" call :install node %ARCH_URL% %ARCH_FOLDER%
+    if     "%INSTALL_LOCALLY%"=="true" call stuff :install
     if NOT "%INSTALL_LOCALLY%"=="true" call :color "%CL_INFO%" "The environment installed on the system is used"
-
-    call :print_color %NPM% -v
-
+    call stuff :version
     goto :eof
 
 :build
     call :color "%CL_HEADER%" "Building client..."
-
-    call :print_color %NPM% -v
-    
-    call :eval_echo "%NPM% install"
-
+    call stuff :version
+    call stuff :build
     goto :eof
 
 :test
     call :color "%CL_HEADER%" "Starting tests..."
-
-    call :eval_echo "%NPM% test"
-
+    call stuff :test
     goto :eof
 
 :run
     call :color "%CL_HEADER%" "Running client..."
-
-    call :eval_echo "%NPM% start %GAME_TO_RUN% %SERVER_URL%"
-
+    call stuff :run
     goto :eof
---
+++ stuff.bat
@@ -0,0 +1,63 @@
+@echo off
+
+call run :init_colors
+
+:check_run_mode
+    if "%*"=="" (       
+        call :run_executable 
+    ) else (
+        call :run_library %*
+    )
+    goto :eof
+
+:run_executable
+    rem run stuff.bat as executable script
+    call run :color ‘%CL_INFO%‘ ‘This is not executable script. Please use 'run.bat' only.‘
+    call run :ask   
+    goto :eof
+
+:run_library
+    rem run stuff.bat as library
+    call %*     
+    goto :eof          
+
+:settings
+    if "%INSTALL_LOCALLY%"=="true" ( set NODE_HOME=)
+
+    if "%NODE_HOME%"==""   ( set NO_NODE=true)
+    if "%NO_NODE%"=="true" ( set NODE_HOME=%ROOT%\.node)
+    if "%NO_NODE%"=="true" ( set PATH=%NODE_HOME%\;%PATH%)
+
+    set NPM=%NODE_HOME%\npm
+
+    echo Language environment variables
+    call run :color ‘%CL_INFO%‘ ‘PATH=%PATH%‘
+    call run :color ‘%CL_INFO%‘ ‘NODE_HOME=%NODE_HOME%‘
+
+    set ARCH_URL=https://nodejs.org/dist/v14.17.0/node-v14.17.0-win-x64.zip
+    set ARCH_FOLDER=node-v14.17.0-win-x64
+    goto :eof
+
+:install
+    if exist %TOOLS%\..\node_modules (
+        call run :eval_echo ‘rd /S /Q %TOOLS%\..\node_modules‘
+    )
+    call run :install node %ARCH_URL% %ARCH_FOLDER%
+    goto :eof
+
+:version
+    call run :print_color %NPM% -v
+    goto :eof
+
+:build
+    call run :eval_echo ‘%NPM% install‘
+    goto :eof
+
+:test    
+    call run :eval_echo ‘%NPM% test‘
+    echo.
+    goto :eof
+
+:run
+    call run :eval_echo ‘%NPM% start %GAME_TO_RUN% %SERVER_URL%‘
+    goto :eof
